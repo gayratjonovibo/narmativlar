b2e8afc4f01242cfb375cdab562f55c7633ed11c
ibrohim_gayradjonov <ghayratjonov@gmail.com>
2026-04-20 22:14:14 +0500

    5-normativ: Custom Register va Login qo‘shildi

---
+++ narmativ5/accounts/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import RegisterForm, LoginForm
+
+def register_view(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password']) # Parolni xavfsiz hashlash
+            user.save()
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = form.cleaned_data['user']
+            login(request, user)
+            return redirect('/') # Asosiy sahifaga
+    else:
+        form = LoginForm()
+    return render(request, 'accounts/login.html', {'form': form})
